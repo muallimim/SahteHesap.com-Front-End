18e88ce194f5aa25a8deb316f4e7b1b5e89431e8
muallimim <muallimim@yahoo.com>
2023-01-15 01:42:28 +0300

    Menü yapıldı

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function menuOnClick() {
+
+    document.getElementById("menu-bar").classList.toggle("change");
+
+    document.getElementById("nav").classList.toggle("change");
+
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    
+  }
